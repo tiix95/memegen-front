2dfb4f3c0a403c8d2b19586d7f9a8a1fa61ed509
Your Name <you@example.com>
2023-06-14 01:48:54 +0200

    100% compatible with docker AND podman, fix Open Redirect?, continue working on uploading custom template

--- front/app/app.py
+++ front/app/app.py
@@ -3,16 +3,16 @@ from flask import json as flask_json
 from dataclasses import dataclass
 from urllib.parse import urlparse
 from hashlib import md5
-from time import time
 import requests
 import json
 import asyncio
 import os
 import sys
+import base64
 from functools import lru_cache
 import cachetools
 
-ttl_cache = cachetools.TTLCache(maxsize=2048, ttl=31 * 24 * 60 * 60)
+ttl_cache = cachetools.TTLCache(maxsize=4096, ttl=31 * 24 * 60 * 60)
 
 
 app = Flask(__name__)
@@ -45,6 +45,15 @@ def get_templates_list():
         _templates_list[temp["id"]] = MemeTemplate(img_url, name, temp["lines"], ext)
     return _templates_list
 
+def filter_path_to_shorten(p):
+    if "//" in p:
+        return False
+    for c in " \n?&%#\\<>\"":
+        if c in p:
+            return False
+    if not p.startswith('/api/images/'):
+        return False
+    return True
 
 
 @app.route('/', methods=["GET"])
@@ -76,11 +85,15 @@ def doc():
 
 @app.route('/shorten', methods=["GET"])
 def shorten():
+    # Maybe Open Redirect here ? Maybe not ?
     global ttl_cache
     p = request.args.get("path")
-    tag = md5((str(time()) + p).encode()).hexdigest()
-    ttl_cache[tag] = p
-    return flask_json.dumps({'path': p, 'tag': tag})
+    if filter_path_to_shorten(p):
+        tag = base64.urlsafe_b64encode(md5(p.encode()).digest()).decode().strip('=')
+        ttl_cache[tag] = p
+        return flask_json.dumps({'path': p, 'tag': tag})
+    else:
+        return 403
 
 @app.route('/meme/<string:tag>', methods=["GET"])
 def short_redirect(tag):
